d2ec7d869c7566b6ee591c130c7299b1b284e9c9
Bogdan-design <bogdanbw@gmail.com>
2023-04-08 13:55:59 +0200

    thunks

--- src/features/auth/auth.selector.ts
+++
@@ -1,5 +0,0 @@
-import {AppRootStateType} from "../../app/store";
-
-export const selectTasks = (state: AppRootStateType) => state.tasks
-export const selectTodolists = (state: AppRootStateType) => state.todolists
-export const selectIsLoggedIn = (state: AppRootStateType) => state.auth.isLoggedIn
